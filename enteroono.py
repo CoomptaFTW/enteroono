741c0c13bf9589e454026e54396bf25987ed15ab
ism41670634 <ism41670634@a11.informatica.escoladeltreball.org>
2018-01-24 12:04:03 +0100

    esentero

---
+++ enteroono.py
@@ -0,0 +1,11 @@
+#coding: utf8
+print ("División")
+divisor = int(input("indique un divisor:"))
+dividendo = int(input("indique un dividendo:"))
+
+if dividendo % divisor == 0:
+	print "entero"
+else:
+	print "no es entero"
+
+
